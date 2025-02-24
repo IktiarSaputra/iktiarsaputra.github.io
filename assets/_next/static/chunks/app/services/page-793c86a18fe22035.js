7c4a953d90a945f249f1c17d14eed09a2ca98f10
Muhammad Ikctiar Saputra <82831537+IktiarSaputra@users.noreply.github.com>
2025-02-24 12:09:12 +0700

    update assets

---
+++ assets/_next/static/chunks/app/services/page-793c86a18fe22035.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[469,236,290,979,13,534],{6502:function(e,n,s){Promise.resolve().then(s.bind(s,9940)),Promise.resolve().then(s.t.bind(s,5250,23))}},function(e){e.O(0,[347,940,971,69,744],function(){return e(e.s=6502)}),_N_E=e.O()}]);
